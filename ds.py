26f935d1c2f739466f9056493ac22d405b39a278
Iskander-afk <64273077+Iskander-afk@users.noreply.github.com>
2020-05-18 19:14:42 +0600

    Update ds.py

--- ds.py
+++ ds.py
@@ -15,7 +15,6 @@ client.remove_command( 'help' )
 
 hello_words = ['hello','Hello','hi','Hi','привет','Привет',]
 question = ['что ты умеешь?','че ты умеешь?','что здесь делать?','че здесь делать?']
-bad = ['Иди нах','иди нах']
 
 @client.event
 
@@ -50,8 +49,6 @@ async def on_message( message ):
         await message.channel.send(f' Здравствуйте {author.mention} чем могу быть полезен? ')
     if msg in question:
         await message.channel.send(f' {author.mention} для полной инфы пропишите команду - !help ')
-    if msg in bad:
-        await message.channel.send(f'WHY YOU BULLING ME? {author.mention} FUCKING BITCH WHY YOU BULLING ME?')
 
 @client.command( pass_context = True )
 
@@ -61,11 +58,10 @@ async def help( ctx ):
     emb.add_field( name = '{}clear'.format ( prefix ), value = 'Очистка чата' )
     emb.add_field( name = '{}kick'.format ( prefix ), value = 'Удаление участника (Только админ) ' )
     emb.add_field( name = '{}time'.format ( prefix ), value = 'Показ времени' )
-    emb.add_field(name='{}cool'.format(prefix), value='с помощью этой команды вы сможете увидеть самое лучшее что вы видели в жизни')
-    emb.add_field(name='{}play'.format(prefix), value='с помощью этой команды вы сможете слушать музыку просто введите команду !play пробел и url музыкального клипа с youtube, также не забудьте зайти в голосовой канал и прописать !join чтобы бот защел и вы его слышали (музыку) и также !leave чтобы бот вышел соответственно')
+    emb.add_field(name='{}play'.format(prefix), value='слушать музыку')
 
     await ctx.send( embed = emb )
-    await ctx.send( 'Все команды писать с префиксом ">" ')
+    await ctx.send( 'Все команды писать с префиксом  ">" ')
 
 @client.command( pass_context = True )
 
@@ -82,20 +78,6 @@ async def time( ctx ):
     emb.add_field( name = 'Показ времени', value = 'Время : {}'.format( now_date ) )
 
     await ctx.send( embed = emb )
-
-@client.command(pass_context=True)
-
-async def cool( ctx ):
-    emb = discord.Embed(title='КЛИКНИ НА ЭТУ НАДПИСЬ И СМОЖЕШЬ УВИДЕТЬ ЧУДО', description='Я ПРОСТО АФИГЕЛ КОГДА УВИДЕЛ ЭТО',colour=discord.Colour.red(), url='https://www.youtube.com/watch?v=9Nqe2TzAZX0')
-
-    emb.set_author(name=client.user.name, icon_url=client.user.avatar_url)
-    emb.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
-    emb.set_image(url='https://i.ytimg.com/vi/9Nqe2TzAZX0/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&amp;rs=AOn4CLAyA6mqCl9DDPZIHmoWxiFZBHN2vw')
-    emb.set_thumbnail(url='https://i.ytimg.com/vi/9Nqe2TzAZX0/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&amp;rs=AOn4CLAyA6mqCl9DDPZIHmoWxiFZBHN2vw')
-
-    emb.add_field(name='ЭТО ПРОСТО ЖЕСТЬ', value='ПОСМОТРИ ПРЯМО СЕЙЧАС')
-
-    await ctx.send(embed=emb)
 
 @client.event
 
