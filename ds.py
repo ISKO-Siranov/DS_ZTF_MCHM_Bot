85e98f94502e522b71fb6c2ba54e126167710056
ISKO-Siranov <64273077+ISKO-Siranov@users.noreply.github.com>
2020-06-11 19:02:47 +0600

    Update ds.py

--- ds.py
+++ ds.py
@@ -124,7 +124,7 @@ async def leave(ctx):
 
 @client.command(pass_context=True)
 async def play(ctx, url: str):
-    server = ctx.Message.server
+    server = ctx.Message.Context.server
     voice_client = client.voice_client_in(server)
     player = await voice_client.create_ytdl_player(url)
     players[server.id] = player
